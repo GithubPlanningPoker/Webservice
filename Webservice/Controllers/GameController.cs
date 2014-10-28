e08ca6f23b342d54f7df794128b40f64a2957a75
mikaelec <mikaelec@gmail.com>
2014-10-28 21:13:51 +0100

    Added routes and methods for all resources

--- Webservice/Controllers/GameController.cs
+++ Webservice/Controllers/GameController.cs
@@ -10,10 +10,10 @@ using Webservice.Models;
 
 namespace Webservice.Controllers
 {
+    [RoutePrefix("api/game")]
     public class GameController : ApiController
     {
-        // POST api/game
-        [Route("api/game")]
+        [Route("")]
         [HttpPost]
         public dynamic createGame([FromBody]String value)
         {
@@ -22,6 +22,58 @@ namespace Webservice.Controllers
                 HostId = getMD5(value + DateTime.Now.ToString()) };
 
             return returnVal;
+        }
+
+        [Route("{gameId:int}/user")]
+        [HttpPost]
+        public dynamic joinGame(int gameId, [FromBody]String value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{gameId:int}/description")]
+        [HttpGet]
+        public dynamic getDescription(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{gameId:int}/description")]
+        [HttpPut]
+        public dynamic updateDescription(int gameId, [FromBody]List<String> value)
+        {
+            String description = value[0]; //TODO not entirely sure that this will work
+            String userId = value[1];
+
+            throw new NotImplementedException();
+        }
+
+        [Route("{gameId:int}/description")]
+        [HttpDelete]
+        public dynamic clearDescription(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{gameId:int}/vote")]
+        [HttpGet]
+        public dynamic getVotes(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{gameId:int}/vote")]
+        [HttpDelete]
+        public dynamic clearVotes(int gameId, [FromBody]String value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{gameId:int}/vote/{userId:int}")]
+        [HttpPost]
+        public dynamic addVote(int gameId, int userId)
+        {
+            throw new NotImplementedException();
         }
 
         private String getMD5(String input)
