99d9ac2584409fb250d0f6a9e4a8b8e97232c3ed
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-28 19:43:27 +0100

    added testcontroller and added method for saving a game in db

--- Webservice/Models/Database.cs
+++ Webservice/Models/Database.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web;
 
 namespace Webservice.Models
 {
     public static class Database
     {
-
+        public void CreateGame(int gameId, int hostId)
+        {
+            var sw = File.AppendText(HttpContext.Current.Server.MapPath("~/App_Data/games.txt"));
+            sw.WriteLine(gameId.ToString(), hostId.ToString());
+        }
     }
 }
