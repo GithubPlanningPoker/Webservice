a5ce6e1b21b26efbfb57ebb813714d1bfdc52d6f
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-29 00:17:21 +0100

    one file per game - much faster than the other model

--- Webservice/Models/Database.cs
+++ Webservice/Models/Database.cs
@@ -3,36 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Webservice.Models
 {
+    public enum Entry
+    {
+        Host = 0,
+        Description = 1,
+        Users = 2,
+        Votes = 3
+    }
+
     public static class Database
     {
+        private static int users = 2;
         private static string gamesPath = HttpContext.Current.Server.MapPath("~/App_Data/games.txt");
         private static string descriptionsPath = HttpContext.Current.Server.MapPath("~/App_Data/descriptions.txt");
         private static string votesPath = HttpContext.Current.Server.MapPath("~/App_Data/votes.txt");
+        private static string filePath = HttpContext.Current.Server.MapPath("~/App_Data/");
+
+        private static string empty = "empty";
 
         public static void CreateGame(string gameId, string hostId)
         {
-            StreamWriter swGames = File.AppendText(gamesPath);
-            swGames.WriteLine(gameId + " " + hostId);
+            StreamWriter swGames = new StreamWriter(filePath + gameId + ".txt");
+            swGames.WriteLine(hostId);
             swGames.Close();
         }
 
         public static void AddUser(string gameId, string userId)
-        {            
-            StreamReader srGames = new StreamReader(gamesPath);
-            List<string> lines = new List<string>();
-            string line;
-            while ((line = srGames.ReadLine()) != null)
-            {
-                if (line.Split(' ')[0] == gameId)
-                    line += " " + userId;
-                lines.Add(line);
-            }
-            Save(lines);
-        }
+        {
+            string fileName = getFile(gameId);
+            string path = filePath + fileName;
+            if (fileName == "")
+                throw new ArgumentException("Game: " + gameId + " does not exist.");
+            string[] lines = File.ReadAllLines(path);
+            lines[2] += userId;
+            save(path, lines);
+        }        
 
         public static void UpdateDescription(string gameId, string description)
         {
@@ -51,7 +61,7 @@ namespace Webservice.Models
             }
             if (!gameExists)
                 lines.Add(gameId + " " + description);
-            Save(lines);
+            //Save(lines);
         }
 
         public static void ClearDescription(string gameId)
@@ -65,7 +75,7 @@ namespace Webservice.Models
                     line = gameId;
                 lines.Add(line);
             }
-            Save(lines);
+            //save(lines)
         }
 
         public static string GetDescription(string gameId)
@@ -99,23 +109,25 @@ namespace Webservice.Models
             return votes;
         }
 
-        private static void Save(List<string> lines)
+        private static void save(string filePath, string[] lines)
         {
-            using (StreamWriter writer = new StreamWriter(gamesPath, false))
+            using (StreamWriter writer = new StreamWriter(filePath, false))
             {
                 foreach (String l in lines)
                     writer.WriteLine(l);
             }
         }
 
-        private static IEnumerable<string> getGameIds()
+
+        private static string getFile(string gameId)
         {
-            StreamReader srGames = new StreamReader(gamesPath);
-            string line;
-            while ((line = srGames.ReadLine()) != null)
+            string file = "";
+            foreach (var fileName in Directory.GetFiles(filePath, "*.txt").Select(Path.GetFileName))
             {
-                yield return line;
+                if (gameId == fileName)
+                    file = fileName;
             }
+            return file;
         }
     }
 }
