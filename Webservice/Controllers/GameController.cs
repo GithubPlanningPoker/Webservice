e4a65c731930b62d7a7c6198a34b14cea90b336b
mikaelec <mikaelec@gmail.com>
2014-10-28 19:22:48 +0100

    Added Game controller and model

---
+++ Webservice/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Webservice.Models;
+
+namespace Webservice.Controllers
+{
+    public class GameController : ApiController
+    {
+        // POST api/<controller>
+        public Game Post([FromBody]String value)
+        {
+            Game game = new Game(value);
+
+            return game;
+        }
+    }
+}
