f9e78779d26244e31b1eeec7ed4cf891d641371c
mikaelec <mikaelec@gmail.com>
2014-11-08 12:52:40 +0100

    Added version controller and GET method

---
+++ Webservice/Controllers/VersionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Webservice.Controllers
+{
+    /// <summary>
+    /// Version number is used by clients to check whether API specfications have changed
+    /// </summary>
+    [RoutePrefix("version")]
+    public class VersionController : ApiController
+    {
+        private const int VERSION_NUMBER = 1;
+
+        /// <summary>
+        /// Gets the latest version number
+        /// </summary>
+        /// <returns>The version number.</returns>
+        [Route("")]
+        [HttpGet]
+        public int getVersion()
+        {
+            return VERSION_NUMBER;
+        }
+    }
+}
